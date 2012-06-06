c2bc7869364c18e874a8d1fde61fcaeee4c40494
Zhe Li <linuxcity.jn@gmail.com>
2012-06-06 20:44:06 +0200

    Added template for "Hello, World"

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import webapp2
+import os
+import jinja2
+
+ROOT_PATH = os.path.dirname(__file__)
+jinja_environment = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.join(ROOT_PATH, 'templates')))
 
 class IndexPage(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello, World!')
+        template_values = {}
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication(
         [('/', IndexPage)],
